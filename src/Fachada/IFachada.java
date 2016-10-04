f277c58bd3bb69de6d84074f12d176c0fc823f66
Rhuan Mota <Rhuan Mota@192.168.25.21>
2016-10-04 17:15:46 -0300

    Início da fachada foi criado. Ainda sem finalização.

---
+++ src/Fachada/IFachada.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fachada;
+
+import Beans.Funcionario;
+import Beans.Produto;
+
+/**
+ *
+ * @author Rhuan Mota
+ */
+public interface IFachada {
+   
+    void inserir(Funcionario funcionario);
+    
+    void procurar (String CpfFuncionario);
+    
+    void remover(String CpfFuncionario);
+    
+    void inserir(Produto produto);
+    
+    void procurar (int idproduto);
+    
+    void remover(int idproduto);
+    
+}
